62cb499310f03ab9e23785b6dadb87d2a2525980
Daniel Segovia <segoarce90@gmail.com>
2024-10-21 09:55:50 -0700

    test form and json

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,3 +1,2 @@
 mod requests;
-mod tasks;
 mod workers;
--- tests/requests/home.rs
+++ tests/requests/home.rs
@@ -1,6 +1,7 @@
+use axum::http::response;
+use form_tester::app::App;
 use insta::assert_debug_snapshot;
 use loco_rs::testing;
-use form_tester::app::App;
 use serial_test::serial;
 
 // TODO: see how to dedup / extract this to app-local test utils
@@ -23,6 +24,44 @@ async fn can_get_home() {
         let notes = request.get("/api").await;
 
         assert_debug_snapshot!((notes.status_code(), notes.text()));
+    })
+    .await;
+}
+
+#[tokio::test]
+#[serial]
+async fn can_post_home_json() {
+    configure_insta!();
+
+    testing::request::<App, _, _>(|request, _ctx| async move {
+        let response = request
+            .post("/api")
+            .json(&serde_json::json!({
+                "name": "Daniel",
+                "age": 99
+            }))
+            .await;
+
+        assert_debug_snapshot!((response.status_code(), response.text()));
+    })
+    .await;
+}
+
+#[tokio::test]
+#[serial]
+async fn can_post_home_form() {
+    configure_insta!();
+
+    testing::request::<App, _, _>(|request, _ctx| async move {
+        let response = request
+            .post("/api")
+            .form(&serde_json::json!({
+                "name": "Daniel",
+                "age": 99
+            }))
+            .await;
+
+        assert_debug_snapshot!((response.status_code(), response.text()));
     })
     .await;
 }
